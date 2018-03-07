dc6abfd54b4b8ded9c5e722eae3223fd0aa703da
Isioye Mohammed <mohammed.isioye@gmail.com>
2018-03-07 16:23:22 +0100

    feature(recycle-view-implementation):
    - imported recycleView widget to gradle file
    - add class to get user details
    
    [Finishes #155738443]

---
+++ app/src/main/java/com/example/isioyemohammed/gitlist/ListItem.java
@@ -0,0 +1,25 @@
+package com.example.isioyemohammed.gitlist;
+
+/**
+ * Created by isioyemohammed on 07/03/2018.
+ */
+
+public class ListItem {
+    private String username;
+    private String company;
+
+    public ListItem(String username, String company) {
+        this.username = username;
+        this.company = company;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+
+}
