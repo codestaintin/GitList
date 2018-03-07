f9fe30a82ae268faee449187a8d96f0e3167faf2
Isioye Mohammed <mohammed.isioye@gmail.com>
2018-03-07 20:24:35 +0100

    feature(connect-view-to-activity):
    - create UserList class for user data
    - create GitList adapter to link data and view together
    
    [Finishes #155768857]

---
+++ app/src/main/java/com/example/isioyemohammed/gitlist/GitListAdapter.java
@@ -0,0 +1,64 @@
+package com.example.isioyemohammed.gitlist;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by isioyemohammed on 07/03/2018.
+ */
+
+public class GitListAdapter extends RecyclerView.Adapter<GitListAdapter.ViewHolder> {
+
+    private List<UserList> userLists;
+    private Context context;
+
+    public GitListAdapter(List<UserList> userLists, Context context) {
+        this.userLists = userLists;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public GitListAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_item, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull GitListAdapter.ViewHolder holder, int position) {
+        holder.setValues(this.userLists.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.userLists.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView textViewUsername;
+        public ImageView imageView;
+        public TextView textViewCompany;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            textViewUsername = (TextView) itemView.findViewById(R.id.textViewUsername);
+            imageView = (ImageView) itemView.findViewById(R.id.imageView);
+            textViewCompany = (TextView) itemView.findViewById(R.id.textViewCompany);
+        }
+
+        public void setValues(@NonNull UserList userList) {
+            this.textViewUsername.setText(userList.getUsername());
+            this.textViewCompany.setText(userList.getCompany());
+        }
+    }
+}
--- app/src/main/java/com/example/isioyemohammed/gitlist/MainActivity.java
+++ app/src/main/java/com/example/isioyemohammed/gitlist/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.isioyemohammed.gitlist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static List<UserList> userLists = new ArrayList<>();
+
+    static {
+        userLists.add(new UserList("Ademola Johnson", "Andela"));
+        userLists.add(new UserList("Issac John", "Southampton"));
+        userLists.add(new UserList("Kobe Bryant", "Man United"));
+        userLists.add(new UserList("Rajon Rondo", "Arsenal"));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RecyclerView mRecyclerView = findViewById(R.id.recyclerView);
+
+        mRecyclerView.setHasFixedSize(true);
+
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        GitListAdapter adapter = new GitListAdapter(userLists, this);
+        mRecyclerView.setAdapter(adapter);
     }
 }
