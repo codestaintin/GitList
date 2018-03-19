0ef1318a2c217bcc948b98ba35db761a17b37391
Isioye Mohammed <mohammed.isioye@gmail.com>
2018-03-19 21:08:34 +0100

    chore(quality-and-syntax-checks):
    - add quality directory
    - add findbugs, checkstyle, lint and pmd ruleset
    - add pre-commit.sh git hook
    - install instapk
    - add javadoc to files to clear code and quality check errors
    
    [Finishes #155965911]

--- app/src/androidTest/java/com/example/isioyemohammed/gitlist/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/isioyemohammed/gitlist/ExampleInstrumentedTest.java
@@ -16,6 +16,10 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    /**
+     * Test - Test suite.
+     * @throws Exception - When an error occurred.
+     */
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
--- app/src/main/java/com/example/isioyemohammed/gitlist/presenter/GithubUsersPresenter.java
+++ app/src/main/java/com/example/isioyemohammed/gitlist/presenter/GithubUsersPresenter.java
@@ -1,8 +1,9 @@
-package com.example.isioyemohammed.gitlist.Presenter;
+package com.example.isioyemohammed.gitlist.presenter;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
 import com.example.isioyemohammed.gitlist.GitListAdapter;
 import com.example.isioyemohammed.gitlist.model.GithubUsers;
@@ -20,36 +21,52 @@ import retrofit2.Response;
  */
 
 public class GithubUsersPresenter {
+    /**
+     * GithubService - creates an instance of GithubService.
+     */
     GithubService githubService;
+    /**
+     * Context - creates an instance of Context.
+     */
     Context context;
 
+    /**
+     * Class method for provided context to the presenter.
+     * @param context - Instance of Context
+     */
     public GithubUsersPresenter(Context context) {
         this.context = context;
-        if(this.githubService == null) {
+        if (this.githubService == null) {
             this.githubService = new GithubService();
         }
     }
 
+    /**
+     * Method for getting list of users from server and populating the recyclerView.
+     * @param recyclerView - recyclerView
+     */
     public void getGitHubUsers(final RecyclerView recyclerView) {
         githubService
                 .getAPI()
                 .getItems()
                 .enqueue(new Callback<GithubUsersResponse>() {
                     @Override
-                    public void onResponse(@NonNull Call<GithubUsersResponse> call, @NonNull Response<GithubUsersResponse> response) {
+                    public void onResponse(@NonNull Call<GithubUsersResponse> call,
+                                           @NonNull Response<GithubUsersResponse> response) {
                         List<GithubUsers> githubUsersResponse = response.body().getItems();
-                        if( githubUsersResponse != null ) {
-                            GitListAdapter adapter = new GitListAdapter(githubUsersResponse, context);
+                        if (githubUsersResponse != null) {
+                            GitListAdapter adapter = new GitListAdapter(githubUsersResponse,
+                                    context);
                             recyclerView.setAdapter(adapter);
                         }
                     }
 
                     @Override
-                    public void onFailure(Call<GithubUsersResponse> call, Throwable t) {
+                    public void onFailure(@NonNull Call<GithubUsersResponse> call, Throwable t) {
                         try {
                             throw new InterruptedException("Oops!! Something went wrong!");
                         } catch (InterruptedException e) {
-                            e.printStackTrace();
+                            Log.e("ERROR", e + "An error occurred");
                         }
                     }
                 });
